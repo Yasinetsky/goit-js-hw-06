ef7a6b5ade51d38d2f12b75a1ff39b9b31c395e7
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-14 00:58:44 +0200

    2ok

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingrListHead = document.querySelector("#ingredients");
+
+const ingredList = ingredients.map(element =>{
+
+  const ingrItem = document.createElement("li");
+  ingrItem.textContent = element;
+  ingrItem.classList.add("item");
+  return ingrItem;
+
+});
+ingrListHead.append(...ingredList);

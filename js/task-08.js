bb4587186576b2ac4d5b1a9e2161d2457cc077af
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-18 18:14:04 +0200

    8ok

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector('form.login-form')
+
+
+
+function loginSubmit(event) {
+    event.preventDefault();
+   if (event.currentTarget.elements.password.value === '' ||
+       event.currentTarget.elements.email.value === ''
+   ) {
+    alert('Все поля должны быть заполнены!');
+    
+   } else {
+    const userData = {
+        email: event.currentTarget.elements.email.value,
+        password: event.currentTarget.elements.password.value,
+   }
+   console.log(userData);
+   event.currentTarget.reset();
+}
+}
+
+
+
+formRef.addEventListener('submit', loginSubmit)
 

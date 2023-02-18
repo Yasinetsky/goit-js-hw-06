4a395d90ac79de38a915874a302d3fabd14a2296
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-18 22:51:21 +0200

    9ок

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonChangeColorRefs = document.querySelector("button.change-color");
+const spanRefs = document.querySelector('span.color')
+
+
+function changeColorBody () {
+  spanRefs.textContent = getRandomHexColor()
+  document.body.style.backgroundColor = getRandomHexColor()
+}
+
+buttonChangeColorRefs.addEventListener('click', changeColorBody)

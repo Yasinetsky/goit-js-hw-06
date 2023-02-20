5e9d6a8cc2f36f44cd987010bc32c7164331b1bc
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-20 15:31:56 +0200

    1fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,31 @@
-const numberOfCategories = document.querySelectorAll("li.item");
-console.log('Number of categories :', numberOfCategories.length);
+// const numberOfCategories = document.querySelectorAll("li.item");
+// console.log('Number of categories :', numberOfCategories.length);
 
-const firstCategory = numberOfCategories[0].querySelector("h2")
-console.log('Category:', firstCategory.textContent)
+// const firstCategory = numberOfCategories[0].querySelector("h2")
+// console.log('Category:', firstCategory.textContent)
 
-const firstCategoryElements = numberOfCategories[0].querySelectorAll("li")
-console.log('Elements:', firstCategoryElements.length)
+// const firstCategoryElements = numberOfCategories[0].querySelectorAll("li")
+// console.log('Elements:', firstCategoryElements.length)
 
-const secondCategory = numberOfCategories[1].querySelector("h2")
-console.log('Category:', secondCategory.textContent)
+// const secondCategory = numberOfCategories[1].querySelector("h2")
+// console.log('Category:', secondCategory.textContent)
 
-const secondCategoryElements = numberOfCategories[1].querySelectorAll("li")
-console.log('Elements:', secondCategoryElements.length)
+// const secondCategoryElements = numberOfCategories[1].querySelectorAll("li")
+// console.log('Elements:', secondCategoryElements.length)
 
-const lastsecondCategory = numberOfCategories[2].querySelector("h2")
-console.log('Category:', lastsecondCategory.textContent)
+// const lastsecondCategory = numberOfCategories[2].querySelector("h2")
+// console.log('Category:', lastsecondCategory.textContent)
 
-const lastCategoryElements = numberOfCategories[2].querySelectorAll("li")
-console.log('Elements:', lastCategoryElements.length)
+// const lastCategoryElements = numberOfCategories[2].querySelectorAll("li")
+// console.log('Elements:', lastCategoryElements.length)
 
+const categories = document.querySelectorAll('#categories .item');
+
+categories.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const elementsNum = category.querySelectorAll('li').length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${elementsNum}`);
+});
+
+console.log(`Number of categories: ${categories.length}`);
